7eb9837d3be50cd2972183ec80101a0f349f5292
aliaahmedraza <aliaahmedraza@gmail.com>
2025-06-26 14:46:07 +0500

    update the phone regression

--- src/validators/twilioValidator.js
+++ src/validators/twilioValidator.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 const twilioValidator = {
     sendOTP: async (req, res, next) => {
         try {
-        const phoneRegExp=/^923(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9])\d{7}$/
+        const phoneRegExp = /^923([0-4][0-9])\d{7}$/;
         const sendOTPSchema = Joi.object({
             recipient:Joi.string().required().pattern(phoneRegExp).min(11).max(13).error(new Error("PhoneNumber must be a valid phone number like 923145209356")),
         })
--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -6,7 +6,7 @@ const userValidator = {
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
         phoneNumber: Joi.string()
-          .pattern(/^923(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9])\d{7}$/)
+          .pattern(/^923([0-4][0-9])\d{7}$/)
           .min(11)
           .max(13)
           .required(),

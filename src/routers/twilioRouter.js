1c2683fd7a38952b5bdac69d006c286fea66cc62
aliaahmedraza <aliaahmedraza@gmail.com>
2025-06-26 14:48:04 +0500

    update valitor middleware of send-otp

--- src/routers/twilioRouter.js
+++ src/routers/twilioRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import twilioController from "../controllers/twilioController.js";
+import twilioValidator from "../validators/twilioValidator.js";
 const twilioRouter = Router();
-twilioRouter.post("/send-otp", twilioController.sendOTP);
+twilioRouter.post("/send-otp",twilioValidator.sendOTP, twilioController.sendOTP);
 twilioRouter.post("/verify-otp", twilioController.verifyOTP);
 export default twilioRouter;
--- src/validators/twilioValidator.js
+++ src/validators/twilioValidator.js
@@ -1,13 +1,21 @@
 import Joi from "joi";
 const twilioValidator = {
-    sendOTP: async (req, res, next) => {
-        try {
-        const phoneRegExp = /^923([0-4][0-9])\d{7}$/;
-        const sendOTPSchema = Joi.object({
-            recipient:Joi.string().required().pattern(phoneRegExp).min(11).max(13).error(new Error("PhoneNumber must be a valid phone number like 923145209356")),
-        })
-    } catch (error) {
-        
-    }
-}
-}
+  sendOTP: async (req, res, next) => {
+    try {
+      const phoneRegExp = /^923([0-4][0-9])\d{7}$/;
+      const sendOTPSchema = Joi.object({
+        recipient: Joi.string()
+          .required()
+          .pattern(phoneRegExp)
+          .min(11)
+          .max(13)
+          .error(
+            new Error(
+              "PhoneNumber must be a valid phone number like 923145209356"
+            )
+          ),
+      });
+    } catch (error) {}
+  },
+};
+export default twilioValidator;
